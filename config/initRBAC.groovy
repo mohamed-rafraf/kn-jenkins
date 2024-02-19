4608030c8bec7dedff8c698d87cd886551a24e86
Mohamed Rafraf <mohamedrafraf@insat.u-carthage.tn>
2024-02-19 08:24:00 +0100

    delete println statements

--- config/initRBAC.groovy
+++ config/initRBAC.groovy
@@ -25,65 +25,35 @@ RoleBasedAuthorizationStrategy rbas = new RoleBasedAuthorizationStrategy()
 // Create admin set of permissions
 Set<Permission> adminPermissions = new HashSet<Permission>();
 adminPermissions.add(Permission.fromId("hudson.model.View.Delete"));
-println "Passing hudson.model.View.Delete"
 adminPermissions.add(Permission.fromId("hudson.model.Computer.Connect"));
-println "Passing hudson.model.Computer.Connect"
 adminPermissions.add(Permission.fromId("hudson.model.Run.Delete"));
-println "Passing hudson.hudson.model.Run.Delete"
 adminPermissions.add(Permission.fromId("com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains"));
-println "Passing com.cloudbees.plugins.credentials.CredentialsProvider.ManageDomains"
 adminPermissions.add(Permission.fromId("hudson.model.Computer.Create"));
-println "Passing hudson.model.Computer.Create"
 adminPermissions.add(Permission.fromId("hudson.model.View.Configure"));
-println "Passing hudson.model.View.Configure"
 adminPermissions.add(Permission.fromId("hudson.model.Computer.Build"));
-println "Passing hudson.model.Computer.Build"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Configure"));
-println "Passing hudson.model.Item.Configure"
 adminPermissions.add(Permission.fromId("hudson.model.Hudson.Administer"));
-println "Passing hudson.model.Hudson.Administer"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Cancel"));
-println "Passing hudson.model.Item.Cancel"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Read"));
-println "Passing hudson.model.Item.Read"
 adminPermissions.add(Permission.fromId("com.cloudbees.plugins.credentials.CredentialsProvider.View"));
-println "Passing com.cloudbees.plugins.credentials.CredentialsProvider.View"
 adminPermissions.add(Permission.fromId("hudson.model.Computer.Delete"));
-println "Passing hudson.model.Computer.Delete"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Build"));
-println "Passing hudson.model.Item.Build"
 adminPermissions.add(Permission.fromId("hudson.scm.SCM.Tag"));
-println "Passing hudson.scm.SCM.Tag"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Discover"));
-println "Passing hudson.model.Item.Discover"
 adminPermissions.add(Permission.fromId("hudson.model.Hudson.Read"));
-println "Passing hudson.model.Hudson.Read"
 adminPermissions.add(Permission.fromId("com.cloudbees.plugins.credentials.CredentialsProvider.Update"));
-println "Passing com.cloudbees.plugins.credentials.CredentialsProvider.Update"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Create"));
-println "Passing hudson.model.Item.Create"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Move"));
-println "Passing hudson.model.Item.Move"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Workspace"));
-println "Passing hudson.model.Item.Workspace"
 adminPermissions.add(Permission.fromId("com.cloudbees.plugins.credentials.CredentialsProvider.Delete"));
-println "Passing com.cloudbees.plugins.credentials.CredentialsProvider.Delete"
 adminPermissions.add(Permission.fromId("hudson.model.View.Read"));
-println "Passing hudson.model.View.Read"
 adminPermissions.add(Permission.fromId("hudson.model.View.Create"));
-println "Passing hudson.model.View.Create"
 adminPermissions.add(Permission.fromId("hudson.model.Item.Delete"));
-println "Passing hudson.model.Item.Delete"
 adminPermissions.add(Permission.fromId("hudson.model.Computer.Configure"));
-println "Passing hudson.model.Computer.Configure"
 adminPermissions.add(Permission.fromId("com.cloudbees.plugins.credentials.CredentialsProvider.Create"));
-println "com.cloudbees.plugins.credentials.CredentialsProvider.Create"
 adminPermissions.add(Permission.fromId("hudson.model.Computer.Disconnect"));
-println "hudson.model.Computer.Disconnect"
 adminPermissions.add(Permission.fromId("hudson.model.Run.Update"));
-println "hudson.model.Run.Update"
 adminPermissions.add(Permission.fromId("hudson.model.Run.Replay"));
-println "Passing hudson.model.Run.Replay"
 
 // Create and add the admin role
 Role adminRole = new Role("admin", adminPermissions)
